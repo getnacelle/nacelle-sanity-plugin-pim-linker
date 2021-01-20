051f38cf7baef41c0dfdf22fa82eb8319aa6bfbc
Nick Richmond <get@nickrichmond.dev>
2021-01-19 20:18:58 -0500

    add components & sanity config

---
+++ src/components/NacelleDataFetcher.js
@@ -0,0 +1,35 @@
+import React, { useContext, useEffect } from 'react'
+import PropTypes from 'prop-types'
+
+import { useHailFrequency } from '../hooks'
+import { SearchOptionsContext } from '../context'
+import Gallery from './Gallery'
+
+const NacelleResults = ({ query, dataHandler, first, after, active }) => {
+  const data = useHailFrequency({ query, dataHandler, first, after })
+  const { setSearchOptions } = useContext(SearchOptionsContext)
+
+  useEffect(() => {
+    if (active) {
+      setSearchOptions(
+        data &&
+          data.map((entry) => ({
+            ...entry,
+            value: entry.title,
+          }))
+      )
+    }
+  }, [data, active, setSearchOptions])
+
+  return <Gallery data={data} active={active} />
+}
+
+NacelleResults.propTypes = {
+  query: PropTypes.string.isRequired,
+  dataHandler: PropTypes.func.isRequired,
+  first: PropTypes.number,
+  after: PropTypes.string,
+  active: PropTypes.bool,
+}
+
+export default NacelleResults
