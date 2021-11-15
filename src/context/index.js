cc32cde2bb5f7d64a93a5d1a4080739a9fb363d2
Alex Asche <alexcasche@gmail.com>
2021-11-15 13:25:24 -0500

    feat: support clearing selected item + add nodemon (#9)
    * feat: support clearing selected item + add nodemon
    
    * chore: adds nodemon to dev deps & enables input

--- src/context/index.js
+++ src/context/index.js
@@ -5,4 +5,9 @@ const SearchOptionsContext = React.createContext([])
 const SearchQueryContext = React.createContext('')
 const SpaceOptionsContext = React.createContext(null)
 
-export { HandleContext, SearchOptionsContext, SearchQueryContext, SpaceOptionsContext }
+export {
+  HandleContext,
+  SearchOptionsContext,
+  SearchQueryContext,
+  SpaceOptionsContext
+}
